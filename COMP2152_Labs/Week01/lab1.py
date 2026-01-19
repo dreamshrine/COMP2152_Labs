c1dc540e1696a630788c6da8515c5bf484172f51
dreamshrine <shayne.e.s.atkins@gmail.com>
2026-01-18 21:02:18 -0500

    Lab 1 Completed

---
+++ COMP2152_Labs/Week01/lab1.py
@@ -0,0 +1,16 @@
+# Sample Coding Questions #1 Week #1
+# Shayne Atkins
+
+array = [1,4,7,9]
+
+a = 1
+b = 2
+c = 3
+d = 4
+e = (a - b) * (c / d) + (a % c)
+
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+userAge = int(input("Please enter your age: ")) + 22
+print("Now showing the shop items filtered by age: ", userAge)
